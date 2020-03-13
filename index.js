a8bb20692fa4c67675d665686c9b7134764767c2
Jason Koo <jkoo2088@gmail.com>
2020-03-13 17:39:21 -0400

    added server code

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.evn.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`listening on ${PORT}`))
